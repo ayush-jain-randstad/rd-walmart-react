041f787c81acd7ddfd71db029191d82036e1d2e9
Biradar <Riyazpatel.Biradar@randstaddigital.com>
2024-09-23 19:58:35 +0530

    issue fixed and minor changes done

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,33 +1,38 @@
-import React, { useState } from 'react'
-import {Input, Button} from '../components/Index.js'
-import { useForm } from 'react-hook-form'
-import authService from '../services/authService.js'
-import { useNavigate } from 'react-router-dom'
+import React, { useState } from "react";
+import { Input, Button } from "../components/Index.js";
+import { useForm } from "react-hook-form";
+import authService from "../services/authService.js";
+import { useNavigate } from "react-router-dom";
 const Login = () => {
-
-  const { register, handleSubmit, formState: {errors} } = useForm()
-  const [error, setError] = useState('')
-  const navigate = useNavigate()
-  const onSubmit = (data) => {    
-      authService.login(data)
-        .then((res) => {
-          if (res.status == 201) {
-            setError(res.message)
-          } else {
-            localStorage.setItem("token", res.token)
-            localStorage.setItem("id", res.userData.id)
-            localStorage.setItem("email", res.userData.email)
-            localStorage.setItem("status", true)
-            navigate('/')
-          }
-        })
-        .catch((error) => setError(error))
-    
-  }
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+  const onSubmit = (data) => {
+    authService
+      .login(data)
+      .then((res) => {
+        if (res.status == 200) {
+          localStorage.setItem("token", res.data.accessToken);
+          localStorage.setItem("id", res.data.id);
+          localStorage.setItem("email", res.data.email);
+          localStorage.setItem("status", true);
+          navigate("/");
+        } else {
+          setError(res?.response?.data?.error?.message || "Bad request");
+        }
+      })
+      .catch((error) => {
+        setError("Something went wrong");
+      });
+  };
 
   return (
     <div>
-        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
             Sign in to your account
@@ -37,38 +42,45 @@ const Login = () => {
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
           <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
             <div>
-              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
                 Email address
               </label>
               <div className="mt-2">
                 <Input
-                  label='Username' 
-                  type='text'
-                  placeholder = "Enter User Name"
+                  label="Username"
+                  type="text"
+                  placeholder="Enter User Name"
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   {...register("username", {
-                    required: 'Username is required',
+                    required: "Username is required",
                     // validate: {
                     //   matcPattern: (value) => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(value)
                     //   || "Email address must be a valid address",
                     // }
                   })}
                 />
-                {errors.email && <p className='text-red-800 mt-4 text-left'>{errors.email.message}</p>}
-                {error && <p className='text-red-800 mt-4 text-left'>{error}</p>}
+                {errors.email && (
+                  <p className="text-red-800 mt-4 text-left">
+                    {errors.email.message}
+                  </p>
+                )}
+                {error && (
+                  <p className="text-red-800 mt-4 text-left">{error}</p>
+                )}
               </div>
             </div>
 
             <div>
               <div className="flex items-center justify-between">
-                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
                   Password
                 </label>
-                <div className="text-sm">
-                  <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-                    Forgot password?
-                  </a>
-                </div>
               </div>
               <div className="mt-2">
                 <Input
@@ -78,10 +90,14 @@ const Login = () => {
                   autoComplete="current-password"
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   {...register("password", {
-                    required: 'Password is required'
+                    required: "Password is required",
                   })}
                 />
-                {errors.password && <p className='text-red-800 mt-4 text-left'>{errors.password.message}</p>}
+                {errors.password && (
+                  <p className="text-red-800 mt-4 text-left">
+                    {errors.password.message}
+                  </p>
+                )}
               </div>
             </div>
 
@@ -93,11 +109,19 @@ const Login = () => {
                 Sign in
               </Button>
             </div>
+            <div className="text-sm text-right">
+              <a
+                href="#"
+                className="font-semibold text-indigo-600 hover:text-indigo-500"
+              >
+                Forgot password?
+              </a>
+            </div>
           </form>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
